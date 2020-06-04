f894d24fd297923b6ea2c07aaa327f980aa25026
root <mailhaim@gmail.com>
2020-06-04 10:19:39 +0000

    updating app.js

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
-
-var express = 		require("express");
-var bodyParser = 	require("body-parser");
-var Campground = 	require("./models/campground");
-var Comment = 		require("./models/comment");
-// var User = 		require("./models/user");
+var express = 			require("express");
+var bodyParser = 		require("body-parser");
+var mongoose = 			require("mongoose");
+var passport =			require("passport");
+var LocalStrategy = 	require("passport-local");
+var Campground = 		require("./models/campground");
+var Comment = 			require("./models/comment");
+var User = 				require("./models/user");
+var session = 			require("express-session"); 
+var methodOverride = 	require("method-override");
 // var seedDB = 		require("./seeds");
-var mongoose = 		require("mongoose");
-
-// Acquiring routes
-var campgroundsRoutes = require("./routes/campgrounds");
-var commentsRoutes = require("./routes/comments");
 
 var app = express();
-app.set("view engine","ejs");
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("public"));
-app.use(campgroundsRoutes);
-app.use(commentsRoutes);
-mongoose.set('useUnifiedTopology', true);
 
-// console.log(process.env.DATABASEURL);
-// mongoose.connect("mongodb://localhost/yelpcamp", { 
-// mongoose.connect("mongodb+srv://haimbuchbut:Adush123@cluster0-dkoym.mongodb.net/test?retryWrites=true&w=majority", { 
+// Acquiring routes
+var campgroundsRoutes 	= require("./routes/campgrounds");
+var commentsRoutes 		= require("./routes/comments");
+var indexRoutes      	= require("./routes/index");
+
+// Connect to the Database (requires to setup DATABASEURL env. var)
+mongoose.set('useUnifiedTopology', true);
 var url = process.env.DATABASEURL || "mongodb://localhost/yelpcamp";
 mongoose.connect(url, { 
 	useNewUrlParser: true,
@@ -30,15 +27,35 @@ mongoose.connect(url, {
 	console.log("Connected to DB!")
 });
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs");
+app.use(express.static(__dirname + "public"));
+app.use(methodOverride('_method'))
+// seedDB(); // Seeding the DB with initial data for testing
 
-
-// Seeding the DB with some data for testing
-// seedDB();
-
-app.get("/", function(req,res){
-	res.render("campgrounds/landing");
+// Configuring passport for authentication
+app.use(require("express-session")({
+	secret: "Leo is fat",
+	resave: false,
+	saveUninitialized : false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate())); 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+// passport.use(User.createStrategy()); enable using different username name (e.g. email). See here: https://www.npmjs.com/package/passport-local-mongoose#simplified-passportpassport-local-configuration
+app.use(function(req,res,next){
+   	res.locals.currentUser = req.user;
+	next();
 });
 
+
+// Routes
+app.use(indexRoutes);
+app.use(campgroundsRoutes);
+app.use(commentsRoutes);
+// Default handling for unavailable pages  
 app.get("*", function(req,res){
 	res.send("This page is not available!");
 });
